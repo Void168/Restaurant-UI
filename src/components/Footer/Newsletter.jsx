cfc905be453f8043c66afe193b98f5003b811bea
LPHung <phuhung16820@gmail.com>
2022-09-23 10:56:47 +0700

    footer

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from 'react'
 
-import './Newsletter.css';
+import './Newsletter.css'
+import SubHeading from '../SubHeading/SubHeading'
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className="app__newsletter">
+    <div className="app__newsletter-heading">
+      <SubHeading title="Thư ngỏ" />
+      <h1 className="headtext__cormorant">Đăng ký để nhận thông báo</h1>
+      <p className="p__opensans">Đừng bỏ lỡ những cập nhật mới nhất!</p>
+    </div>
+    <div className="app__newsletter-input flex__center">
+      <input type="email" placeholder="Địa chỉ email của bạn" />
+      <button type="button" className="custom__button">
+        Đăng ký
+      </button>
+    </div>
   </div>
-);
+)
 
-export default Newsletter;
+export default Newsletter
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,11 +1,57 @@
-import React from 'react';
+import React from 'react'
 
-import './Footer.css';
+import './Footer.css'
+import { FooterOverlay, Newsletter } from '../../components'
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi'
+import { images } from '../../constants'
 
 const Footer = () => (
-  <div>
-    Footer
-  </div>
-);
+  <div className="app__footer section__padding" id="login">
+    <FooterOverlay />
+    <Newsletter />
 
-export default Footer;
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Liên hệ với chúng tôi</h1>
+        <p className="p__opensans">
+          869 Âu Cơ, phường Tân Sơn Nhì, Tân Phú, Hồ Chí Minh
+        </p>
+        <p className="p__opensans">0123456789</p>
+        <p className="p__opensans">0234567891</p>
+      </div>
+
+      <div className="app__footer-links_logo">
+        <img src={images.gericht} alt="footer_logo" />
+        <p className="p__opensans">
+          &quot;Cách tốt nhất để tìm lại chính mình là đánh mất chính mình để
+          phục vụ người khác.&quot;
+        </p>
+        <img
+          src={images.spoon}
+          className="spoon__img"
+          style={{ marginTop: 15 }}
+          alt="footer img"
+        />
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
+      </div>
+
+      <div className="app__footer-links_work">
+        <h1 className="app__footer-headtext">Giờ mở cửa</h1>
+        <p className="p__opensans">Thứ hai - Thứ sáu:</p>
+        <p className="p__opensans">08:00 am - 12:00 am</p>
+        <p className="p__opensans">Thứ bảy - Chủ nhật:</p>
+        <p className="p__opensans">07:00 am - 11:00 pm</p>
+      </div>
+    </div>
+
+    <div className="footer__copyright">
+      <p className="p__opensans">2022 Gericht. All Rights reserved.</p>
+    </div>
+  </div>
+)
+
+export default Footer
